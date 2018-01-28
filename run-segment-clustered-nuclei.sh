c776fbce9aaf007a9546ae99d35889569d1c04f5
Ulysse Rubens <ulyrubens@gmail.com>
2018-01-28 18:50:11 +0100

    Update run-segment-clustered-nuclei.sh

--- run-segment-clustered-nuclei.sh
+++ run-segment-clustered-nuclei.sh
@@ -5,7 +5,7 @@ Xvfb $DISPLAY -auth /dev/null &
 (
 # the '(' starts a new sub shell. In this sub shell we start the worker processes:
 
-java -Xmx6000m -cp /fiji/jars/ij-1.51s.jar ij.ImageJ -headless --console -macro NucleiSegmentation-ImageJ.ijm "input=$1, output=$2, radius=$3, threshold=$4"
+java -Xmx6000m -cp /fiji/jars/ij-1.51s.jar ij.ImageJ -headless --console -macro /fiji/NucleiSegmentation-ImageJ.ijm "input=$1, output=$2, radius=$3, threshold=$4"
 wait # waits until all 'program' processes are finished
 # this wait sees only the 'program' processes, not the Xvfb process
 )
