5e39d27f572820ffff56c7c42a194bb353b1af46
rmaree <raphael.maree@ulg.ac.be>
2018-01-29 11:25:12 +0100

    Create add_cytomine_software.py

---
+++ add_cytomine_software.py
@@ -0,0 +1,26 @@
+import cytomine
+import sys
+
+#Connect to cytomine, edit connection values
+
+cytomine_host="my-host"  # Cytomine core URL
+cytomine_public_key="my-public-key"  # Your public key
+cytomine_private_key="my-private-key" # Your private key
+id_project=my-project-id
+ 
+#Connection to Cytomine Core
+conn = cytomine.Cytomine(cytomine_host, cytomine_public_key, cytomine_private_key, base_path = '/api/', working_path = '/tmp/', verbose= True)
+ 
+execute_command = "python algo/ij_segment_clustered_nuclei/NucleiSegmentation-ImageJ-wrapper.py --ij_radius $ij_radius --ij_threshold $ij_threshold " + "--cytomine_host $host " + "--cytomine_public_key $publicKey " +"--cytomine_private_key $privateKey " + "--cytomine_id_project $cytomine_id_project "
+
+#define software parameter template
+software = conn.add_software("IJSegmentClusteredNuclei", "createRabbitJobWithArgsService","ValidateAnnotation", execute_command)
+conn.add_software_parameter("ij_radius", software.id, "Number", 5, True, 10, False)
+conn.add_software_parameter("ij_threshold", software.id, "Number", -0.5, True, 30, False)
+ 
+#for logging (set by server)
+conn.add_software_parameter("cytomine_id_software", software.id, "Number",0, True, 400, True)
+conn.add_software_parameter("cytomine_id_project", software.id, "Number", 0, True, 500, True)
+
+#add software to a given project
+addSoftwareProject = conn.add_software_project(id_project,software.id)
