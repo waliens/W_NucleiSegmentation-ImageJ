b816d62e1a3498cf2bdfa7982074a163c05878cf
Romain Mormont <romainmormont@hotmail.com>
2018-07-09 15:35:30 +0200

    update to 1.52d version

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ Xvfb $DISPLAY -auth /dev/null &
 
 #Not sure it is the good ij version we got from fiji automatic installation
 #should fix the version during installation or find the actual jar file that has been installed
-java -Xmx6000m -cp /fiji/jars/ij-1.51s.jar ij.ImageJ -headless --console -macro macro.ijm "input=$1, output=$2, radius=$3, threshold=$4"
+java -Xmx6000m -cp /fiji/jars/ij-1.52d.jar ij.ImageJ --headless --console -macro macro.ijm "input=$1, output=$2, radius=$3, threshold=$4"
 wait # waits until all 'program' processes are finished
 # this wait sees only the 'program' processes, not the Xvfb process
 )
