53a9a6cccbb97d6bd5fdfbb99d1fe87deac80c3a
Romain Mormont <romainmormont@hotmail.com>
2018-07-09 11:49:46 +0200

    revert run.sh file

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 # Run the metrics-macro in ImageJ headlessly using Xvfb to avoid errors due to ImageJ1 gui dependencies.
 
-cd /fiji && ./ImageJ-linux64 --headless --console -macro macro.ijm "input=$1, output=$2, radius=$3, threshold=$4"
+export DISPLAY=:2
+Xvfb $DISPLAY -auth /dev/null &
+(
+# the '(' starts a new sub shell. In this sub shell we start the worker processes:
+
+#Not sure it is the good ij version we got from fiji automatic installation
+#should fix the version during installation or find the actual jar file that has been installed
+java -Xmx6000m -cp /fiji/jars/ij-1.51s.jar ij.ImageJ -headless --console -macro macro.ijm "input=$1, output=$2, radius=$3, threshold=$4"
+wait # waits until all 'program' processes are finished
+# this wait sees only the 'program' processes, not the Xvfb process
+)
