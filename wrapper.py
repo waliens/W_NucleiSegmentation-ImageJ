777def95eec6a7a388699b21aa44f6f94cbd7806
Waliens <romainmormont@hotmail.com>
2018-11-14 18:01:16 +0100

    update wrapper - use generic function to simplify implementation

--- wrapper.py
+++ wrapper.py
@@ -3,12 +3,10 @@ import sys
 from subprocess import call
 
 from cytomine import CytomineJob
-from cytomine.models import Annotation, Job, ImageInstanceCollection, AnnotationCollection, Property
-from shapely.affinity import affine_transform
-from skimage import io
+from cytomine.models import Job
 
-from annotation_exporter import mask_to_objects_2d
 from neubiaswg5.metrics import computemetrics_batch
+from neubiaswg5.cytomine import prepare_data, upload_data, upload_metrics
 
 
 def main(argv):
@@ -16,40 +14,11 @@ def main(argv):
     with CytomineJob.from_cli(argv) as cj:
         cj.job.update(status=Job.RUNNING, progress=0, statusComment="Initialisation...")
 
-        # 1. Create working directories on the machine:
-        # - WORKING_PATH/in: input images
-        # - WORKING_PATH/out: output images
-        # - WORKING_PATH/ground_truth: ground truth images
-        # - WORKING_PATH/tmp: temporary path
-        base_path = "{}".format(os.getenv("HOME"))
-        gt_suffix = "_lbl"
-        working_path = os.path.join(base_path, str(cj.job.id))
-        in_path = os.path.join(working_path, "in")
-        out_path = os.path.join(working_path, "out")
-        gt_path = os.path.join(working_path, "ground_truth")
-        tmp_path = os.path.join(working_path, "tmp")
+        problem_cls = "ObjSeg"
 
-        if not os.path.exists(working_path):
-            os.makedirs(working_path)
-            os.makedirs(in_path)
-            os.makedirs(out_path)
-            os.makedirs(gt_path)
-            os.makedirs(tmp_path)
-
-        # 2. Download the images (first input, then ground truth image)
-        cj.job.update(progress=1, statusComment="Downloading images (to {})...".format(in_path))
-        image_instances = ImageInstanceCollection().fetch_with_filter("project", cj.parameters.cytomine_id_project)
-        input_images = [i for i in image_instances if gt_suffix not in i.originalFilename]
-        gt_images = [i for i in image_instances if gt_suffix in i.originalFilename]
-
-        for input_image in input_images:
-            input_image.download(os.path.join(in_path, "{id}.tif"))
-
-        for gt_image in gt_images:
-            related_name = gt_image.originalFilename.replace(gt_suffix, '')
-            related_image = [i for i in input_images if related_name == i.originalFilename]
-            if len(related_image) == 1:
-                gt_image.download(os.path.join(gt_path, "{}.tif".format(related_image[0].id)))
+        # 1. Create working directories on the machine
+        # 2. Download the images
+        in_images, gt_images, in_path, gt_path, out_path, tmp_path = prepare_data(problem_cls, cj)
 
         # 3. Call the image analysis workflow using the run script
         cj.job.update(progress=25, statusComment="Launching workflow...")
@@ -62,42 +31,13 @@ def main(argv):
             cj.job.update(progress=50, statusComment=err_desc)
             raise ValueError(err_desc)
 
-        # 4. Upload the annotation and labels to Cytomine (annotations are extracted from the mask using
-        # the AnnotationExporter module)
-        for image in cj.monitor(input_images, start=60, end=80, period=0.1, prefix="Extracting and uploading polygons from masks"):
-            file = "{}.tif".format(image.id)
-            path = os.path.join(out_path, file)
-            data = io.imread(path)
-
-            # extract objects
-            slices = mask_to_objects_2d(data)
-
-            print("Found {} polygons in this image {}.".format(len(slices), image.id))
-
-            # upload
-            collection = AnnotationCollection()
-            for obj_slice in slices:
-                collection.append(Annotation(
-                    location=affine_transform(obj_slice.polygon, [1, 0, 0, -1, 0, image.height]).wkt,
-                    id_image=image.id, id_project=cj.parameters.cytomine_id_project, property=[
-                        {"key": "index", "value": str(obj_slice.label)}
-                    ]
-                ))
-            collection.save()
+        # 4. Upload the annotation and labels to Cytomine
+        upload_data(problem_cls, cj, in_images, out_path,
+                    start=60, end=90, period=0.1, prefix="Extracting and uploading polygons from masks")
 
         # 5. Compute and upload the metrics
-        cj.job.update(progress=80, statusComment="Computing and uploading metrics...")
-        outfiles, reffiles = zip(*[
-            (os.path.join(out_path, "{}.tif".format(image.id)),
-             os.path.join(gt_path, "{}.tif".format(image.id)))
-            for image in input_images
-        ])
-
-        results = computemetrics_batch(outfiles, reffiles, "ObjSeg", tmp_path)
-
-        for key, value in results.items():
-            Property(cj.job, key=key, value=str(value)).save()
-        Property(cj.job, key="IMAGE_INSTANCES", value=str([im.id for im in input_images])).save()
+        cj.job.update(progress=90, statusComment="Computing and uploading metrics...")
+        upload_metrics(problem_cls, cj, in_images, gt_path, out_path, tmp_path)
 
         # 6. End
         cj.job.update(status=Job.TERMINATED, progress=100, statusComment="Finished.")
