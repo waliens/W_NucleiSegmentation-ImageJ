56c17ed2431e7ec881fcaf426a0c94192c0118dc
Waliens <romainmormont@hotmail.com>
2018-11-19 17:29:48 +0100

    modify to handle nodownload, noexport and nometrics flags

--- wrapper.py
+++ wrapper.py
@@ -1,46 +1,48 @@
-import os
 import sys
 from subprocess import call
 
-from cytomine import CytomineJob
 from cytomine.models import Job
 
-from neubiaswg5.metrics import computemetrics_batch
+from neubiaswg5 import CLASS_OBJSEG
+from neubiaswg5.cytomine import NeubiasJob
 from neubiaswg5.cytomine import prepare_data, upload_data, upload_metrics
 
 
 def main(argv):
-    # 0. Initialize Cytomine client and job
-    with CytomineJob.from_cli(argv) as cj:
-        cj.job.update(status=Job.RUNNING, progress=0, statusComment="Initialisation...")
+    # 0. Initialize Cytomine client and job if necessary and parse inputs
+    with NeubiasJob.from_cli(argv) as nj:
+        nj.job.update(status=Job.RUNNING, progress=0, statusComment="Initialisation...")
 
-        problem_cls = "ObjSeg"
+        problem_cls = CLASS_OBJSEG
 
         # 1. Create working directories on the machine
         # 2. Download the images
-        in_images, gt_images, in_path, gt_path, out_path, tmp_path = prepare_data(problem_cls, cj)
+        in_images, gt_images, in_path, gt_path, out_path, tmp_path = prepare_data(problem_cls, nj, is_2d=True, **nj.flags)
 
         # 3. Call the image analysis workflow using the run script
-        cj.job.update(progress=25, statusComment="Launching workflow...")
+        nj.job.update(progress=25, statusComment="Launching workflow...")
         command = "/usr/bin/xvfb-run java -Xmx6000m -cp /fiji/jars/ij.jar ij.ImageJ --headless --console " \
-                  "-macro macro.ijm \"input={}, output={}, radius={}, threshold={}\"".format(in_path, out_path, cj.parameters.ij_radius, cj.parameters.ij_threshold)
+                  "-macro macro.ijm \"input={}, output={}, radius={}, threshold={}\"".format(in_path, out_path, nj.parameters.ij_radius, nj.parameters.ij_threshold)
         return_code = call(command, shell=True, cwd="/fiji")  # waits for the subprocess to return
 
         if return_code != 0:
             err_desc = "Failed to execute the ImageJ macro (return code: {})".format(return_code)
-            cj.job.update(progress=50, statusComment=err_desc)
+            nj.job.update(progress=50, statusComment=err_desc)
             raise ValueError(err_desc)
 
         # 4. Upload the annotation and labels to Cytomine
-        upload_data(problem_cls, cj, in_images, out_path,
-                    start=60, end=90, period=0.1, prefix="Extracting and uploading polygons from masks")
+        upload_data(problem_cls, nj, in_images, out_path, **nj.flags, monitor_params={
+            "start": 60, "end": 90,
+            "period": 0.1,
+            "prefix": "Extracting and uploading polygons from masks"
+        })
 
         # 5. Compute and upload the metrics
-        cj.job.update(progress=90, statusComment="Computing and uploading metrics...")
-        upload_metrics(problem_cls, cj, in_images, gt_path, out_path, tmp_path)
+        nj.job.update(progress=90, statusComment="Computing and uploading metrics...")
+        upload_metrics(problem_cls, nj, in_images, gt_path, out_path, tmp_path, **nj.flags)
 
         # 6. End
-        cj.job.update(status=Job.TERMINATED, progress=100, statusComment="Finished.")
+        nj.job.update(status=Job.TERMINATED, progress=100, statusComment="Finished.")
 
 
 if __name__ == "__main__":
